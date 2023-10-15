200c8c1b35fae715e2fc8391a12c40132cc88987
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-10-16 01:47:28 +0900

    init

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
